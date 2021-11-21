380c1f01156e1639e37724de7933e0e0dd652f15
wook <twins1040@8percent.kr>
2021-11-21 22:38:36 +0900

    ADD: layer setting

--- tesla/settings.py
+++ tesla/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tesla.wsgi.application'
 ASGI_APPLICATION = "tesla.asgi.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
