77d4194847807cac710321438485203cbec91111
wook <twins1040@8percent.kr>
2021-11-21 21:58:57 +0900

    ADD: asgi setting

--- tesla/settings.py
+++ tesla/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tesla.wsgi.application'
+ASGI_APPLICATION = "tesla.asgi.application"
 
 
 # Database
